4b3ab51751b443f20e29b335987e67b081ef443d
olvifarrand <olivia.farrand@edu.salpaus.fi>
2024-03-06 11:06:09 +0200

    Tehtävä 4 valmis

--- main.js
+++ main.js
@@ -16,10 +16,10 @@ document.getElementById("aika").innerHTML = kellonaika;
 
 
 // Teht 2, Guessing game
-let button = document.getElementById('button');
+let guessButton = document.getElementById('guessButton');
 let vastaus = document.getElementById('vastaus');
 
-  button.onclick = function randomFunc() {
+  guessButton.onclick = function randomFunc() {
     let input = document.getElementById('numero').value;
     var randomnum = Math.floor(Math.random() * 10 +1);
 
@@ -35,6 +35,32 @@ let vastaus = document.getElementById('vastaus');
 let kertobutton = document.getElementById('kertonappi');
 let jakobutton = document.getElementById('jakonappi');
 
-let numero1 = document.getElementById('ekaNum')
-let numero2 = document.getElementById('tokaNum')
+  kertobutton.onclick = function KertoLasku() {
+    let numero1 = document.getElementById('ekaNum').value;
+    let numero2 = document.getElementById('tokaNum').value;
+    kertolasku = numero1 * numero2;
+    tulos.innerHTML = kertolasku;
+  }
+
+  jakobutton.onclick = function JakoLasku() {
+    let numero1 = document.getElementById('ekaNum').value;
+    let numero2 = document.getElementById('tokaNum').value;
+    jakolasku = numero1 / numero2;
+    tulos.innerHTML = jakolasku;
+  }
+
+
+// Teht 4, 
+// Kaava : c/5 = (f-32)/9
+let lämpöButton = document.getElementById('lämpöNappi');
+let CtoF = document.getElementById('CtoF');
+let FtoC = document.getElementById('FtoC');
+
+  lämpöButton.onclick = function lämpötilaLasku() {
+    let numb = document.getElementById('lämpötila').value;
+    CtoF = numb * 1.8 + 32;
+    FtoC = (numb -32) / 1.8;
+    CtoFandFtoC.innerHTML = numb + '°C on ' + CtoF + '°F<br>' + numb + '°F on ' + FtoC + '°C'
+  }
+
 
